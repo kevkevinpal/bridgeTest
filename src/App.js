11e23157cfefb0adb12d26eade6d31cba25f65a0
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-08-18 09:15:47 -0600

    made a simple page to authroize and send sats with keysend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as sphinx from "sphinx-bridge";
+import { useState } from "react";
 
 function App() {
+  const [satsSent, setSatsSent] = useState(0);
+  const [keysendRes, setKeysendRes] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+								Sats sent {satsSent} <br/><br/>
+          keysendRes: {JSON.stringify(keysendRes)}
         </a>
-      </header>
+        <button
+          style={{ height: "50px" }}
+          onClick={async () => await sphinx.enable()}
+        >
+          Click here to authorize
+        </button>
+        <button
+          style={{ height: "50px" }}
+          onClick={async () => {
+            const res = await sphinx.keysend(
+              "023d8eb306f0027b902fbdc81d33b49b6558b3434d374626f8c324979c92d47c21",
+              5
+            );
+            setKeysendRes(res);
+            if (res != null && res.success == true) {
+              setSatsSent(satsSent + 5);
+            }
+          }}
+        >
+          Click here to send 5 sats to
+          023d8eb306f0027b902fbdc81d33b49b6558b3434d374626f8c324979c92d47c21
+        </button>
     </div>
   );
 }
